6203ff17ba29b2929988205605a95319d96f6973
Abdali21 <160420279+Abdali21@users.noreply.github.com>
2025-03-06 15:31:14 +0000

    add style to register form and login form

--- resources/views/profile/register.blade.php
+++ resources/views/profile/register.blade.php
@@ -1,16 +1,54 @@
 <x-master title="register">
-    <form method="POST" action="{{route("profiles.store")}}">
-        @csrf
-        <label>Name</label>
-        <input type="text" name="name" placeholder="enter your name">
-        <label>email</label>
-        <input type="mail" name="email" placeholder="example@gmail.com" >
-        <label>Password</label>
-        <input type="password" name="password" placeholder="enter your password">
-        <label>Password confirmation</label>
-        <input type="password" name="password_confirmation" placeholder="enter your password">
-        <button type="submit">
-            Register
-        </button>
-    </form>
+    <div class="h-screen flex justify-center items-center">
+        <form method="POST" action="{{route("profiles.store")}}" class="w-96 p-6 bg-gray-100">
+            @csrf
+            <h2 class="text-2xl text-center font-semibold mb-4">Register</h2>
+            <div class="flex flex-col space-y-1  mb-4">
+                <label class="font-semibold">Name :</label>
+                <input type="text" name="name" placeholder="Enter your name" 
+                class="p-2 border border-gray-300 rounded-md"
+                >
+                @error('name')
+                   <span class="text-red-400 mt-1">
+                    {{$message}}
+                   </span>
+                @enderror
+            </div>
+
+            <div class="flex flex-col space-y-1  mb-4">
+                <label class="font-semibold">Email :</label>
+                <input type="mail" name="email" placeholder="example@gmail.com"
+                 class="p-2 border border-gray-300 rounded-md"
+                >
+                @error('email')
+                <span class="text-red-400 mt-1">
+                 {{$message}}
+                </span>
+             @enderror
+            </div>
+
+            <div class="flex flex-col space-y-1  mb-4">
+                <label class="font-semibold">Password : </label>
+                <input type="password" name="password" placeholder="enter your password"
+                 class="p-2 border border-gray-300 rounded-md"
+                >
+                @error('password')
+                <span class="text-red-400 mt-1">
+                 {{$message}}
+                </span>
+             @enderror
+            </div>
+
+            <div class="flex flex-col space-y-1 mb-4">
+                <label class="font-semibold">Password Confirmation :</label>
+                <input type="password" name="password_confirmation" placeholder="enter your password"
+                 class="p-2 border border-gray-300 rounded-md"
+                >
+            </div>
+
+            <button class="w-full py-2 text-gray-100 bg-blue-500 hover:bg-blue-600 rounded-md cursor-pointer" type="submit">
+                Register
+            </button>
+        </form>
+    </div>
 </x-master>
--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -2,9 +2,9 @@
     <div class="flex items-center justify-center h-screen">
     <form method="POST" action="{{route("login.login")}}" class="bg-gray-100 p-8 w-96 rounded-md ">
         @csrf
-        <h2 class="text-center text-2xl font-semibold mb-4">User Login</h2>
+        <h2 class="text-center text-2xl font-semibold mb-4">Login</h2>
 
-        <div class="flex flex-col justify-center space-y-2 mb-4">
+        <div class="flex flex-col justify-center space-y-1 mb-4">
             <label class="font-semibold" for="email">Email :</label>
             <input name="email" type="email" placeholder="Example@gmail.com"
             class="w-full p-2 border border-gray-300  rounded-md ">
@@ -15,7 +15,7 @@
             @enderror
         </div>
 
-        <div class="flex flex-col justify-center space-y-2 mb-4">
+        <div class="flex flex-col justify-center space-y-1 mb-4">
             <label for="password" class="font-semibold ">Password :</label>
             <input name="password" type="password" placeholder="Enter your password"
             class="w-full p-2 border border-gray-400  rounded-md">
